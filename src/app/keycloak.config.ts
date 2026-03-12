999d6fdfa4bdc4d374369360664582833b4c1db0
radar2 <securkydroity@gmail.com>
2026-03-12 10:24:46 +0000

    Change keycloak config pattern

--- src/app/keycloak.config.ts
+++ src/app/keycloak.config.ts
@@ -10,8 +10,8 @@ import {
 import { environment } from '../environments/environment';
 
 const localhostCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
-  // urlPattern: /^(https:\/\/payment.banianpay.com)(\/.*)?$/i
-  urlPattern: /^(http:\/\/localhost:8090)(\/.*)?$/i
+  urlPattern: /^(https:\/\/payment.banianpay.com)(\/.*)?$/i
+  // urlPattern: /^(http:\/\/localhost:8090)(\/.*)?$/i
 });
 
 export const provideKeycloakAngular = () =>
